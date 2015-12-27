023253311bb0fee3d19234228b3a6654d183ee41
Kouki Saito <dan.addr.skd@gmail.com>
2015-12-27 18:09:30 +0900

    Initial Commit

---
+++ Menthas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Menthas
+//
+//  Created by 斉藤　洸紀　 on 2015/12/27.
+//  Copyright © 2015年 Kouki Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
